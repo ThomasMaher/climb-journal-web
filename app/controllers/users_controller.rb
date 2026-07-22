e6ee42d1d9479b4dface3dcbe64174c6f3b34a17
Thomas Maher <Thomasmaher1210@gmail.com>
2026-07-21 23:27:51 -0400

    Seed db and add in break for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def home_stats
     user = User.includes(:sessions, session_climbs: :boulder).find_by(id: params[:user_id])
+    render :json, status: :not_found and return unless user.present?
+
     @user_stats = UserStatsService.new(user)
     render json: { overall: @user_stats.run, past_month: @user_stats.run(days_ago: 30) }
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,102 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+tom = User.create(id: 1, first_name: 'Tom', last_name: 'Maher')
+
+first_session = Session.create(gym_name: 'Vital', date: '2025-12-10', user_id: tom.id)
+Session.create(gym_name: 'GP81', date: '2026-05-10', user_id: tom.id)
+Session.create(gym_name: 'Metrorock', date: Time.zone.today - 16.days, user_id: tom.id)
+last_session = Session.create(gym_name: 'Vital', date: '2026-07-19', user_id: tom.id)
+
+b1 = Boulder.create(
+  nickname: 'Salmon-reach',
+  vgrade_range_min: 6,
+  vgrade_range_max: 7,
+  self_grade: 7,
+  incline: 0,
+  boulder_type: 'Indoor'
+)
+b4 = Boulder.create(
+  nickname: 'volume-slab',
+  vgrade_range_min: 4,
+  vgrade_range_max: 5,
+  self_grade: 4,
+  incline: 0,
+  boulder_type: 'Indoor'
+)
+b2 = Boulder.create(
+  nickname: 'Blue-swing-dyno',
+  vgrade_range_min: 4,
+  vgrade_range_max: 5,
+  self_grade: 4,
+  incline: 35,
+  boulder_type: 'Indoor'
+)
+b3 = Boulder.create(
+  nickname: 'Spooky-yellow-slab',
+  vgrade_range_min: 6,
+  vgrade_range_max: 7,
+  self_grade: 6,
+  incline: 0,
+  boulder_type: 'Indoor'
+)
+b5 = Boulder.create(
+  nickname: 'Yellow-volume-slab',
+  vgrade_range_min: 1,
+  vgrade_range_max: 2,
+  self_grade: 3,
+  incline: 0,
+  boulder_type: 'Indoor'
+)
+
+sc = SessionClimb.create(
+  session_id: last_session.id,
+  user_id: tom.id,
+  boulder_id: b1.id,
+  attempts: 7,
+  percent_finished: 25,
+  warmup: false,
+)
+
+SessionClimb.create(
+  session_id: last_session.id,
+  user_id: tom.id,
+  boulder_id: b2.id,
+  attempts: 3,
+  percent_finished: 100,
+  warmup: false,
+)
+SessionClimb.create(
+  session_id: last_session.id,
+  user_id: tom.id,
+  boulder_id: b3.id,
+  attempts: 3,
+  percent_finished: 100,
+  warmup: false,
+)
+SessionClimb.create(
+  session_id: last_session.id,
+  user_id: tom.id,
+  boulder_id: b4.id,
+  attempts: 4,
+  percent_finished: 100,
+  warmup: false,
+)
+SessionClimb.create(
+  session_id: last_session.id,
+  user_id: tom.id,
+  boulder_id: b5.id,
+  attempts: 2,
+  percent_finished: 100,
+  warmup: true,
+)
+
+SessionClimb.create(
+  session_id: first_session.id,
+  user_id: tom.id,
+  boulder_id: b1.id,
+  attempts: 8,
+  percent_finished: 15,
+  warmup: false,
+)
